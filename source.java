7748a7bca013892326c2e29caceed52b3602e4ed
Ana Carolina Farah <anacarolfarah@gmail.com>
2020-08-01 02:20:00 -0300

    Update source.java

--- source.java
+++ source.java
@@ -1,16 +1,20 @@
 import java.util.*;
+
 class Solution {
-public int solution(int[] A) {
-Arrays.sort(A);
-for (int i = 0, j = 1; i < A.length; i++, j++) {
-if (j < A.length && A[i] == A[j]){
-i++;
-j++;
-}
-else {
-return A[i];
-}
-}
-return 0;
-}
+     public int solution(int[] A) {
+  
+  Arrays.sort(A);
+
+       for (int i = 0, j = 1; i < A.length; i++, j++) {
+           if (j < A.length && A[i] == A[j]){
+               i++;
+               j++;
+          }
+           else {
+                return A[i];
+      }
+         }
+    
+       return 0;
+    }
 }
